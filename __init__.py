d7a580889685bc25aa9c277e7aba1896d85386c3
electronyan <electronyan@tencent.com>
2024-06-26 17:58:27 +0800

    更换实现方式 减少了代码量

--- __init__.py
+++ __init__.py
@@ -2,28 +2,9 @@ from binaryninja import *
 from binaryninjaui import UIContext
 
 
-def highLightBlock_high(bv: BinaryView, inst: HighLevelILInstruction):
-    func = inst.function.source_function
-    hlil = inst.function
-
-    ctx = UIContext.activeContext()
-    h = ctx.contentActionHandler()
-    a = h.actionContext()
-    token_state = a.token
-    var = Variable.from_identifier(func, token_state.token.value)
-    uses = hlil.get_var_uses(var)
-    defines = hlil.get_var_definitions(var)
-    references = uses + defines
-    references = list(set(references))
-    for ref in references:
-        block = hlil.get_basic_block_at(ref.instr_index)
-        block.highlight = HighlightStandardColor.BlueHighlightColor
-
-
-def resetBlockHighlight_high(bv: BinaryView, inst: HighLevelILInstruction):
-    func = inst.function.source_function
-    hlil = inst.function
-
+def highLightBlock(bv: BinaryView, func: Function):
+    hlil = func.hlil
+    mlil = func.mlil
     ctx = UIContext.activeContext()
     h = ctx.contentActionHandler()
     a = h.actionContext()
@@ -36,77 +17,37 @@ def resetBlockHighlight_high(bv: BinaryView, inst: HighLevelILInstruction):
     references = list(set(references))
     for ref in references:
         block = hlil.get_basic_block_at(ref.instr_index)
-        block.highlight = HighlightStandardColor.NoHighlightColor
-
-
-def highLightBlock_middle(bv: BinaryView, inst: MediumLevelILInstruction):
-    func = inst.function.source_function
-    mlil = inst.function
-
-    ctx = UIContext.activeContext()
-    h = ctx.contentActionHandler()
-    a = h.actionContext()
-    token_state = a.token
-    var = Variable.from_identifier(func, token_state.token.value)
-    uses =  mlil.get_var_uses(var)
-    defines = mlil.get_var_definitions(var)
-    references = uses + defines
-    references = list(set(references))
-    for ref in references:
-        block = mlil.get_basic_block_at(ref.instr_index)
+        if not block:
+            continue
         block.highlight = HighlightStandardColor.BlueHighlightColor
-
-
-def resetBlockHighlight_middle(bv: BinaryView, inst: MediumLevelILInstruction):
-    func = inst.function.source_function
-    mlil = inst.function
-
-    ctx = UIContext.activeContext()
-    h = ctx.contentActionHandler()
-    a = h.actionContext()
-    token_state = a.token
-    var = Variable.from_identifier(func, token_state.token.value)
-
     uses = mlil.get_var_uses(var)
     defines = mlil.get_var_definitions(var)
     references = uses + defines
     references = list(set(references))
-
     for ref in references:
         block = mlil.get_basic_block_at(ref.instr_index)
-        block.highlight = HighlightStandardColor.NoHighlightColor
+        if not block:
+            continue
+        block.highlight = HighlightStandardColor.BlueHighlightColor
 
 
-def resetAllBlockHighlight(bv: BinaryView, inst: HighLevelILInstruction):
-    hlil = inst.function
-    func = inst.function.source_function
+def resetAllBlockHighlight(bv: BinaryView, func: Function):
+    hlil = func.hlil
     mlil = func.mlil
     for block in hlil.basic_blocks:
         block.highlight = HighlightStandardColor.NoHighlightColor
     for block in mlil.basic_blocks:
         block.highlight = HighlightStandardColor.NoHighlightColor
+
+
 def isV(bv: BinaryView, inst: HighLevelILInstruction):
     return True
 
 
-PluginCommand.register_for_high_level_il_instruction(
-    "highLight\\hightlight", "", highLightBlock_high, isV)
-
-PluginCommand.register_for_high_level_il_instruction(
-    "highLight\\undoHightLight", "", resetBlockHighlight_high, isV)
+PluginCommand.register_for_function(
+    "highLight\\hightlight", "", highLightBlock, isV)
 
 
-PluginCommand.register_for_high_level_il_instruction(
+PluginCommand.register_for_function(
     "highLight\\resetAll", "", resetAllBlockHighlight, isV)
 
-
-
-
-PluginCommand.register_for_medium_level_il_instruction(
-    "highLight\\hightlight ", "", highLightBlock_middle, isV)
-
-PluginCommand.register_for_medium_level_il_instruction(
-    "highLight\\undoHightLight ", "", resetBlockHighlight_middle, isV)
-
-PluginCommand.register_for_medium_level_il_instruction(
-    "highLight\\resetAll ", "", resetAllBlockHighlight, isV)
